4ca7c0e516bc7bb37f36607fe106575f90d2acc2
asep jumadi <asepjumadi27@gmail.com>
2019-08-28 09:21:55 +0700

    update new fonts

--- public/js/app.js
+++ public/js/app.js
@@ -49366,8 +49366,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/asep/laravelprt1/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/asep/laravelprt1/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/asep/learninglaravel1/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/asep/learninglaravel1/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
